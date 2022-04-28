8483d217fc531159048581f73beb28981c9b79ed
Хохлова Наталия <nataliebasket@gmail.com>
2022-04-28 21:04:04 +0300

    1

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -6,23 +6,17 @@ set -e
 git config user.name "Хохлова Наталия"
 git config user.email "nataliebasket@gmail.com"
 
-# echo "Start to deploy..."
-
-# git checkout --orphan gh-pages
-# ls -l
-
-# git --work-tree ./dist add --all
-# git --work-tree ./dist commit -m "Deploy to GitHub Pages"
-# git push -f origin HEAD:gh-pages
-# rm -r dist
-
-# git checkout -f master
-# git branch -D gh-pages
-
+echo "Start to deploy..."
 
 git checkout --orphan gh-pages
-cp -R dist/* ./
-git add index.html assets/
-git commit -m 'update gh-pages' && git push
+ls -l
+
+git --work-tree ./dist add --all
+git --work-tree ./dist commit -m "Deploy to GitHub Pages"
+git push -f origin HEAD:gh-pages
+rm -r dist
+
+git checkout -f master
+git branch -D gh-pages
 
 echo "Successfully deployed"
