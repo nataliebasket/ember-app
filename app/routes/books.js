b3e6a3739296b192f97230f39600c7d661e9b8bf
Хохлова Наталия <nataliebasket@gmail.com>
2022-05-03 09:44:14 +0300

    работа с моделью

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  dataService: service('data'), //с ипользованием inject получаем нужный сервис
+
+  model () {
+    return this.get("dataService").getBooks();
+  }
 });
